3e5b55bb942eb01f307eb3fedc25a9c4a8094dec
jon-shogren <jon.shogren@mailchimp.com>
2022-08-03 20:18:24 -0400

    working search, returns current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
 const key = "eb3cb13f1dbdafda62142b6699ac4aa6";
 var city = '';
 
+function getWeather() {
+    city = $('#search-city').val();
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + key;
+    fetch(url)
+    .then((response) => {
+         return response.json();
+    })
+    .then((data) => {
+        console.log(data.main.temp);
+        let currentWeatherHTML = `
+            <h3>${data.name}</h3>
+            <ul class="list-unstyled">
+                <li>Temperature: ${data.main.temp}&#8457;</li>
+                <li>Humidity: ${data.main.humidity}%</li>
+                <li>Wind Speed: ${data.wind.speed} mph</li>
+            </ul>`;
+        // Append the results to the DOM
+        $('#current-weather').html(currentWeatherHTML);
+    })
+
+
+} 
+
+// getWeather();
+// city search button event listener
+$('#search-button').on("click", (event) => {
+    event.preventDefault();
+    city = $('#search-city').val();
+    getWeather(event);
+    });
